a9edaa6e315aeb34009e0d43d5fd53b5f621451f
JacobLinCool <jacoblincool@gmail.com>
2021-10-20 01:00:41 +0800

    fix(page): change GitHub link url

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className="App">
             <div className="GitHub">
-                <a href="" target="_blank">
+                <a href="https://github.com/JacobLinCool/NTNU-Finder" target="_blank">
                     <h1>GitHub</h1>
                 </a>
             </div>
